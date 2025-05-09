061c031ceeefb5c833c74ada60da366bc9afc1c7
Lucianosolari <95387477+Lucianosolari@users.noreply.github.com>
2025-05-09 19:27:48 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,20 @@
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
 
 def test_suma():
     assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
     assert suma(0.3, 0.7) == 1
+
+
+def test_resta():
+    assert resta(5, 2) == 3
+    assert resta(1, 1) == 1
+
 
 
 
